cbc778d65264871f1384c9e11f96b4ccbf8dbd0f
uchitosato <uchitosato@gmail.com>
2023-06-06 23:04:18 +0900

    refactor code

--- extension.js
+++ extension.js
@@ -21,7 +21,7 @@ function activate(context) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from myfirstextension!');
+		vscode.window.showInformationMessage('Hello VS Code!');
 	});
 
 	context.subscriptions.push(disposable);
